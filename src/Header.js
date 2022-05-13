6a967fdc1a002410b2cf96912530c49ad10fe55d
Justin <justinmathiue0@gmail.com>
2022-05-12 18:19:56 -0700

    started lab 06

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <>
+                <h1>City Explorer</h1>
+                <h2>Learn about cities...</h2>
+
+            </>
+        );
+    }
+}
+
+export default Header;
