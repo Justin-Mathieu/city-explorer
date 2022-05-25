9091b7dc60c1fd2069410bf2ddab658f7b07b1ac
Justin <justinmathiue0@gmail.com>
2022-05-25 01:44:37 -0700

    addded some styling...

--- src/Movie.js
+++ src/Movie.js
@@ -13,25 +13,24 @@ class Weather extends React.Component {
         return (
             <>
                 {this.props.movieinfo.map(card =>
-                    < Container >
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500${card.image}`} alt={card.title} />
-                            <Card.Body>
-                                <Card.Title>{card.title}</Card.Title>
-                                <Card.Text>
-                                    {card.overview}
-                                </Card.Text>
-                            </Card.Body>
-                            <ListGroup className="list-group-flush">
-                                <ListGroupItem>Release Date{card.release}</ListGroupItem>
-                                <ListGroupItem>Average Votes: {card.avgVotes}</ListGroupItem>
-                                <ListGroupItem>Popularity: {card.pop}</ListGroupItem>
-                            </ListGroup>
-                            <Card.Body>
-                            </Card.Body>
-                        </Card>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img className='img' variant="top" src={`https://image.tmdb.org/t/p/w500${card.image}`} alt={card.title} />
+                        <Card.Body className='card'>
+                            <Card.Title>{card.title}</Card.Title>
+                            <Card.Text>
+                                {card.overview}
+                            </Card.Text>
+                        </Card.Body>
+                        <ListGroup className="list-group-flush">
+                            <ListGroupItem>Release Date{card.release}</ListGroupItem>
+                            <ListGroupItem>Average Votes: {card.avgVotes}</ListGroupItem>
+                            <ListGroupItem>Popularity: {card.pop}</ListGroupItem>
+                        </ListGroup>
+                        <Card.Body>
+                        </Card.Body>
+                    </Card>
 
-                    </Container>)}
+                )}
             </>
         );
     }
